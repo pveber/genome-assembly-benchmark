bc04499e869e58b510b3e9e0b1e8adf94ad24f90
Philippe Veber <philippe.veber@gmail.com>
2016-02-15 15:39:59 +0100

    spades: added package

--- src/spades.ml
+++ src/spades.ml
@@ -3,6 +3,22 @@ open Bistro.Std
 open Bistro_bioinfo.Std
 open Bistro.EDSL_sh
 
+
+let package = Bistro.Workflow.make ~descr:"spades.package" [%sh{|
+PREFIX={{ dest }}
+
+mkdir -p $PREFIX
+cd $PREFIX
+wget http://spades.bioinf.spbau.ru/release3.6.2/SPAdes-3.6.2.tar.gz
+tar xvfz SPAdes-3.6.2
+cd SPAdes-3.6.2
+./spades_compile.sh
+cp -r bin ..
+cp -r share/spades ../share
+cd ..
+rm -rf SPAdes-3.6.2*
+|}]
+
 let pe_args (ones, twos) =
   let opt side i x =
     opt (sprintf "--pe%d-%d" (i + 1) side) dep x
@@ -23,7 +39,7 @@ let spades
   =
   workflow ~descr:"spades" [
     mkdir_p dest ;
-    cmd "spades.py" [
+    cmd ~path:[package] "spades.py" [
       option (flag string "--sc") single_cell ;
       option (flag string "--iontorrent") iontorrent ;
       option pe_args pe ;
