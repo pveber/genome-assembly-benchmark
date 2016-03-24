72a7d7f269fb65867468cf13bc657eae65e8b591
pveber <philippe.veber@gmail.com>
2016-03-24 19:09:50 +0100

    fixed memory and proc consumption for SPAdes
    closes #1

--- src/spades.ml
+++ src/spades.ml
@@ -21,11 +21,13 @@ let spades
     ()
   : spades_output workflow
   =
-  workflow ~descr:"spades" [
+  workflow ~np:4 ~mem:(4 * 1024) ~descr:"spades" [
     mkdir_p dest ;
     cmd "spades.py" [
       option (flag string "--sc") single_cell ;
       option (flag string "--iontorrent") iontorrent ;
+      opt "--threads" ident np ;
+      opt "--memory" (fun m -> seq [ string "$((" ; mem ; string " / 1024))" ]) mem ;
       option pe_args pe ;
       opt "-o" ident dest ;
     ]
